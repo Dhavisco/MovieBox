ae9a968e2b3990556ffc1e5e9123734fa835b8c3
David Ayomide Olaniyi <63981998+Dhavisco@users.noreply.github.com>
2023-09-16 20:34:20 +0100

    Update request.js

--- request.js
+++ request.js
@@ -3,8 +3,7 @@
    export const header = {
       method:"GET",
       headers:{
-        "Content-Type": "application/json",
-        "Authorization": "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI5M2UzZDcxY2RjZGNiOGFlOGQ2NDBjYWYwYThhYTBjNyIsInN1YiI6IjY0ZmZkNzdiZDdkY2QyMDBmZmViNGJiMyIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.VHa1UEAfuCLv0UyG9lRE2BRxMw-SQQ1Zm1tUOlciImM"
+        "Content-Type": "application/json"
       }
       
     }

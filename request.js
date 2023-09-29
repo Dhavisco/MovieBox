3ae6ac3b83be68f19938f812df0e1fa03b6a8abd
petsamuel <petsamuel4@gmail.com>
2023-09-29 11:17:44 +0100

    chore: reduce redundancy

--- request.js
+++ request.js
@@ -1,10 +1,28 @@
+import home from "./src/assets/Home.svg";
+import moviesprojector from "./src/assets/Movie Projector.svg";
+import tv from "./src/assets/TV Show.svg";
 
+const menuLists = [
+  {
+    name: "Home",
+    logo: home,
+    link: "/",
+  },
+  {
+    name: "Movies",
+    logo: moviesprojector,
+    link: "#",
+  },
+  {
+    name: "Tv Series",
+    logo: tv,
+    link: "#",
+  },
+  {
+    name: "Upcomimg",
+    logo: tv,
+    link: "#",
+  },
+];
 
-   export const header = {
-      method:"GET",
-      headers:{
-        "Content-Type": "application/json"
-      }
-      
-    }
-
+export { menuLists };
